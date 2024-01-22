75c377c45bbaf91a0725fc14c2f022867e010050
mahitmehta <mahit.py@gmail.com>
2024-01-22 00:25:36 -0400

    Static

--- x264-sys/build.rs
+++ x264-sys/build.rs
@@ -19,6 +19,8 @@ fn main() {
     let headers = x264.include_paths.clone();
     let buildver = x264.version.split('.').nth(1).unwrap();
 
+    println!("cargo:rustc-link-lib=static=x264");
+
     let mut builder = bindgen::builder()
         .raw_line(format!(
             "pub unsafe fn x264_encoder_open(params: *mut x264_param_t) -> *mut x264_t {{
