3fe1d6db696cbd5cd26921078248dfbe5cab0ced
Mahit Mehta <mahit.py@gmail.com>
2024-01-26 22:24:55 +0000

    Linux Static Linking

--- x264-sys/build.rs
+++ x264-sys/build.rs
@@ -12,15 +12,27 @@ fn format_write(builder: bindgen::Builder) -> String {
         .replace("/*!", "/*")
 }
 
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
 fn main() {
     let libs = system_deps::Config::new().probe().unwrap();
     // TODO pass include paths to bindgen
     let x264 = libs.get_by_name("x264").unwrap();
     let headers = x264.include_paths.clone();
     let buildver = x264.version.split('.').nth(1).unwrap();
-
+   
+    if cfg!(target_os = "linux") && cfg!(target_arch = "aarch64") {
+        println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu/");
+    } else if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") {
+        println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
+    } 
+    
     println!("cargo:rustc-link-lib=static=x264");
-
+    
     let mut builder = bindgen::builder()
         .raw_line(format!(
             "pub unsafe fn x264_encoder_open(params: *mut x264_param_t) -> *mut x264_t {{
